37f5aaf4a349f769c68ea15bda9112836e2ac9fe
Alexey Orlov <orlov@bos-ltd.ru>
2015-12-05 12:19:22 +0300

    Afternoon 4

--- game.js
+++ game.js
@@ -19,6 +19,7 @@ BasicGame.Game.prototype = {
     this.setupEnemies();
     this.setupBullets();
     this.setupExplosions();
+    this.setupPlayerIcons();
     this.setupText();
 
     this.cursors = this.input.keyboard.createCursorKeys();
@@ -43,6 +44,10 @@ BasicGame.Game.prototype = {
       var enemy = this.enemyPool.getFirstExists(false);
       // spawn at a random location top of the screen
       enemy.reset(this.rnd.integerInRange(20, this.game.width - 20), 0);
+      enemy.reset(
+        this.rnd.integerInRange(20, this.game.width - 20), 0,
+        BasicGame.ENEMY_HEALTH
+      );
       // also randomize the speed
       enemy.body.velocity.y = this.rnd.integerInRange(
         BasicGame.ENEMY_MIN_Y_VELOCITY,
@@ -75,7 +80,11 @@ BasicGame.Game.prototype = {
 
     if (this.input.keyboard.isDown(Phaser.Keyboard.Z) ||
         this.input.activePointer.isDown) {
-      this.fire();
+      if (this.returnText && this.returnText.exists) {
+        this.quitGame();
+      } else {
+        this.fire();
+      }
     }
   },
 
@@ -83,19 +92,59 @@ BasicGame.Game.prototype = {
     if (this.instructions.exists && this.time.now > this.instExpire) {
       this.instructions.destroy();
     }
+
+    if (this.ghostUntil && this.ghostUntil < this.time.now) {
+      this.ghostUntil = null;
+      this.player.play('fly');
+    }
+
+    if (this.showReturn && this.time.now > this.showReturn) {
+      this.returnText = this.add.text(
+        this.game.width / 2, this.game.height / 2 + 20, 
+        'Press Z or Tap Game to go back to Main Menu', 
+        { font: '16px sans-serif', fill: '#fff'}
+      );
+      this.returnText.anchor.setTo(0.5, 0.5);
+      this.showReturn = false;
+    }
   },
 
   enemyHit: function (bullet, enemy) {
     bullet.kill();
-    this.explode(enemy);
-    enemy.kill();
+    this.damageEnemy(enemy, BasicGame.BULLET_DAMAGE);
   },
   
   playerHit: function (player, enemy) {
-    this.explode(enemy);
-    enemy.kill();
-    this.explode(player);
-    player.kill();
+    this.damageEnemy(enemy, BasicGame.CRASH_DAMAGE);
+    var life = this.lives.getFirstAlive();
+    if (life !== null) {
+      life.kill();
+      this.ghostUntil = this.time.now + BasicGame.PLAYER_GHOST_TIME;
+      this.player.play('ghost');
+    } else {
+      this.explode(player);
+      player.kill();
+      this.displayEnd(false);
+    }
+  },
+
+  damageEnemy: function (enemy, damage) {
+    enemy.damage(damage);
+    if (enemy.alive) {
+      enemy.play('hit');
+    } else {
+      this.explode(enemy);
+      this.addToScore(enemy.reward);
+    }
+  },
+
+  addToScore: function (score) {
+    this.score += score;
+    this.scoreText.text = this.score;
+    if (this.score >= 2000) {
+      this.enemyPool.destroy();
+      this.displayEnd(true);
+    }
   },
 
   fire: function() {
@@ -143,6 +192,22 @@ BasicGame.Game.prototype = {
     this.game.debug.body(this.player);
   },
 
+  displayEnd: function (win) {
+    // you can't win and lose at the same time
+    if (this.endText && this.endText.exists) {
+      return;
+    }
+
+    var msg = win ? 'You Win!!!' : 'Game Over!';
+    this.endText = this.add.text( 
+      this.game.width / 2, this.game.height / 2 - 60, msg, 
+      { font: '72px serif', fill: '#fff' }
+    );
+    this.endText.anchor.setTo(0.5, 0);
+
+    this.showReturn = this.time.now + BasicGame.RETURN_MESSAGE_DELAY;
+  },
+
   //
   // create()- related functions
   //
@@ -155,6 +220,7 @@ BasicGame.Game.prototype = {
     this.player = this.add.sprite(this.game.width / 2, this.game.height - 50, 'player');
     this.player.anchor.setTo(0.5, 0.5);
     this.player.animations.add('fly', [ 0, 1, 2 ], 20, true);
+    this.player.animations.add('ghost', [3, 0, 3, 1], 20, true);
     this.player.play('fly');
     this.physics.enable(this.player, Phaser.Physics.ARCADE);
     this.player.speed = BasicGame.PLAYER_SPEED;
@@ -162,7 +228,18 @@ BasicGame.Game.prototype = {
     // 20 x 20 pixel hitbox, centered a little bit higher than the center
     this.player.body.setSize(20, 20, 0, -5);
   },
- 
+  
+  setupPlayerIcons: function () {
+    this.lives = this.add.group();
+    // calculate location of first life icon
+    var firstLifeIconX = this.game.width - 10 - (BasicGame.PLAYER_EXTRA_LIVES * 30);
+    for (var i = 0; i < BasicGame.PLAYER_EXTRA_LIVES; i++) {
+      var life = this.lives.create(firstLifeIconX + (30 * i), 30, 'player');
+      life.scale.setTo(0.5, 0.5);
+      life.anchor.setTo(0.5, 0.5);
+    }
+  },
+
   setupEnemies: function () {
     this.enemyPool = this.add.group();
     this.enemyPool.enableBody = true;
@@ -172,10 +249,15 @@ BasicGame.Game.prototype = {
     this.enemyPool.setAll('anchor.y', 0.5);
     this.enemyPool.setAll('outOfBoundsKill', true);
     this.enemyPool.setAll('checkWorldBounds', true);
+    this.enemyPool.setAll('reward', BasicGame.ENEMY_REWARD, false, false, 0, true);
 
     // Set the animation for each sprite
     this.enemyPool.forEach(function (enemy) {
       enemy.animations.add('fly', [ 0, 1, 2 ], 20, true);
+      enemy.animations.add('hit', [3, 1, 3, 2], 20, false);
+      enemy.events.onAnimationComplete.add( function (e) {
+        e.play('fly');
+      }, this);
     });
 
     this.nextEnemyAt = 0;
@@ -229,16 +311,30 @@ BasicGame.Game.prototype = {
     );
     this.instructions.anchor.setTo(0.5, 0.5);
     this.instExpire = this.time.now + BasicGame.INSTRUCTION_EXPIRE;
+
+    this.score = 0;
+    this.scoreText = this.add.text(
+      this.game.width / 2, 30, '' + this.score, 
+      { font: '20px monospace', fill: '#fff', align: 'center' }
+    );
+    this.scoreText.anchor.setTo(0.5, 0.5);
   },
 
   quitGame: function (pointer) {
 
     //  Here you should destroy anything you no longer need.
     //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
-
+    this.sea.destroy();
+    this.player.destroy();
+    this.enemyPool.destroy();
+    this.bulletPool.destroy();
+    this.explosionPool.destroy();
+    this.instructions.destroy();
+    this.scoreText.destroy();
+    this.endText.destroy();
+    this.returnText.destroy();
     //  Then let's go back to the main menu.
     this.state.start('MainMenu');
-
   }
 
 };
